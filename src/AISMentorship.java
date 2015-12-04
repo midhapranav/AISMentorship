ab7d1d5e187f6e8f8c0137c20d83e37fb5ab87c1
Pranav Midha <midhapranav@Pranavs-MacBook-Pro.local>
2015-12-03 20:57:15 -0800

    First peice of code

--- src/AISMentorship.java
+++ src/AISMentorship.java
@@ -8,46 +8,87 @@ import java.util.List;
 public class AISMentorship {
 
     public static int MAX_MENTEE_PER_MENTOR = 10;
-    static class mentors {
+
+    static class Mentors {
         String name;
         String email;
         String departement;
         String specialization;
         int menteeCount;
-        List<mentees>menteeNames;
+        List<Mentees>menteeNames;
     }
-    static class mentees {
+    static class Mentees {
         String name;
         String email;
         String department;
         String specialization;
     }
 
+    public static List<Mentors> mentorList = new ArrayList<Mentors>();
+    public static List<Mentees> unMappedmenteeList = new ArrayList<Mentees>();
+    public static HashMap<Mentees, Mentors> mappedMenteesMap = new HashMap<Mentees, Mentors>();
+
     public static void main(String args[]) {
-        List<mentors> mentorList = new ArrayList<mentors>();
-        List<mentees> unMappedmenteeList = new ArrayList<mentees>();
-        HashMap<mentees, mentors> mappedMenteesMap = new HashMap<mentees, mentors>();
+        mentorList = new ArrayList<Mentors>();
+        unMappedmenteeList = new ArrayList<Mentees>();
+        mappedMenteesMap = new HashMap<Mentees, Mentors>();
         //TODO populate mentorList and menteeList from dummy data
 
         /*
          * The algorithm below is a brute algorithm, better implementation will be a bucket sort.
          */
-        for(mentors mentor : mentorList) {
+        for(Mentors mentor : mentorList) {
             if(mentor.menteeCount < MAX_MENTEE_PER_MENTOR) {
-                //call a function which adds a mentee from the same field
-                //add mentee to the mentor's mentee list
-                //remove mentee from unMappedMenteeList
-                // add mentee to mapped mentee map
+                Mentees newMentee = findSuitableMentee(mentor);
+                mentor.menteeNames.add(newMentee);
+                mappedMenteesMap.put(newMentee, mentor);
+                //unMappedmenteeList.remove(mentee);
+                //the above line may throw Concurrent Array Modification exception, if not remove the checks on the hashMap
+                //and uncomment this line
             }
         }
 
-        for(mentees mentee : unMappedmenteeList) {
-            //find a mentor with same field as mentee
-            //add mentee to the mentor's mentee list
-            //remove mentee from unmapped mentee list
-            // add mentee to mapped mentee map
+        //TODO sort mentorList based on number of mentees assigned
+
+        for(Mentees mentee : unMappedmenteeList) {
+            if (!mappedMenteesMap.containsKey(mentee)) {
+                Mentors newMentor = findSuitableMentor(mentee);
+                newMentor.menteeNames.add(mentee);
+                mappedMenteesMap.put(mentee, newMentor);
+                //unMappedmenteeList.remove(mentee);
+                //the above line may throw Concurrent Array Modification exception, if not remove the checks on the hashMap
+                //and uncomment this line
+            } else {
+                //do-nothing
+            }
         }
 
         //create a email thread for all mentors and cc all the mentees in it.
+    }
+
+    private static Mentees findSuitableMentee(Mentors mentor) {
+        Mentees newMentee = new Mentees();
+        for(Mentees mentee : unMappedmenteeList) {
+            if(!mappedMenteesMap.containsKey(mentee)) {
+                if (mentee.department.equals(mentor.departement)) {
+                    newMentee = mentee;
+                    break;
+                }
+            } else {
+                //do nothing
+            }
+        }
+        return newMentee;
+    }
+
+    private static Mentors findSuitableMentor(Mentees mentee) {
+        Mentors newMentor = new Mentors();
+        for(Mentors mentor : mentorList) {
+            if(mentee.department.equals(mentor.departement)) {
+                newMentor = mentor;
+                break;
+            }
+        }
+        return newMentor;
     }
 }
